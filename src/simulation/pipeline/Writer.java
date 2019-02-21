ab549981068c9fbe9f88ddb78d63d9cc179b1490
cwdinehart1 <37390080+cwdinehart1@users.noreply.github.com>
2019-02-21 18:32:27 -0500

    Update Writer.java

--- src/simulation/pipeline/Writer.java
+++ src/simulation/pipeline/Writer.java
@@ -12,17 +12,21 @@ import java.util.ArrayList;
  *
  */
 public class Writer extends PipelineSegment{
-
+    
     private Register memwbRegister;
-    private ArrayList<Boolean> fields;
-    private boolean regWrite;
-    private boolean memToReg;
-    private String  aluResult;
-    private String  memData;
-    private String  rdField;
-    private Register[] regs;
+    private ArrayList<Boolean> fields; // holds the info from the control unit  
+    private boolean regWrite; // tells write if we are writing to a register
+    private boolean memToReg; // tells write if we are writing info from memory
+    private String  aluResult;// the result from the alu in Execute
+    private String  memData;  // data recieved from memory
+    private String  rdField;  // the destination register number
+    private Register[] regs;  // the array of registers
 
 
+   /**
+    * Contructor for Write, takes the memwb pipeline register, and the 
+    * register array from the simulator.
+    */
     public Writer(Register memwb, Register[] regs) {
         this.memwbRegister = memwb;
         this.regWrite = false;
@@ -34,7 +38,8 @@ public class Writer extends PipelineSegment{
     }
 
     /**
-     *
+     * Method that recieves the flags from the control unit and the data 
+     * from the memwb register.
      */
     private void read(){
         fields = ControlUnit.getControlInstructions(4);
@@ -47,10 +52,16 @@ public class Writer extends PipelineSegment{
     }
 
     /**
-     *
+     * Method that checks the flags recieved from the control unit
+     * and determines whether we are writing data from memory 
+     * or data from the alu result into the destination register.
      */
     private void write(){
+        //if regWrite is true then we are writing to a register, 
+        //else we do nothing
         if(regWrite){
+            //if memToReg is true then we are writing data from memory to
+            //the destination register, else write the alu result.
             if(memToReg){
                 regs[Integer.parseInt(rdField,2)].writeBinary(memData);
             }else{
@@ -60,7 +71,8 @@ public class Writer extends PipelineSegment{
     }
 
     /**
-     *
+     * Method first checks to see if the control unit will let this stage 
+     * run, then if it does run, runs read() and execute().
      */
     public void execute(){
         if(ControlUnit.getGoAhead(4)){
