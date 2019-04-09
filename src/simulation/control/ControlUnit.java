b14289a68bba228ca780b27deb5f1ff1510b350b
MinoForge <rendrag6@bellsouth.net>
2019-04-08 22:48:01 -0400

    ControlUnit todo with branch with link

--- src/simulation/control/ControlUnit.java
+++ src/simulation/control/ControlUnit.java
@@ -154,6 +154,7 @@ public class ControlUnit {
             unit.flags.set(4,DEASSERT); //Branch
             unit.flags.set(5,DEASSERT); //MemRead
             unit.flags.set(6,DEASSERT); //MemWrite
+            //TODO if branch with link, assert this. Else, deasssert.
             unit.flags.set(7,ASSERT);   //RegWrite
             unit.flags.set(8,DEASSERT); //Mem2Reg
 
